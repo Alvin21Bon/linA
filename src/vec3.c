4fcd9b4e2a407ce437381ad2ebd89bd4d5f86b48
Alvin21Bon <alvin21bon@gmail.com>
2023-10-25 00:28:21 -0400

    add and subtract functions

--- src/vec3.c
+++ src/vec3.c
@@ -7,8 +7,18 @@ void fillVec3(const float x, const float y, const float z, Vec3 outputVector)
 	outputVector[2] = z;
 }
 
-void addVec3(const Vec3 vectorA, const Vec3 vectorB, Vec3 outputVector);
-void subtractVec3(const Vec3 vectorA, const Vec3 vectorB, Vec3 outputVector);
+void addVec3(const Vec3 vectorA, const Vec3 vectorB, Vec3 outputVector)
+{
+	outputVector[0] = vectorA[0] + vectorB[0];
+	outputVector[1] = vectorA[1] + vectorB[1];
+	outputVector[2] = vectorA[2] + vectorB[2];
+}
+void subtractVec3(const Vec3 vectorA, const Vec3 vectorB, Vec3 outputVector)
+{
+	outputVector[0] = vectorA[0] - vectorB[0];
+	outputVector[1] = vectorA[1] - vectorB[1];
+	outputVector[2] = vectorA[2] - vectorB[2];
+}
 
 void sumOfVec3(Vec3 outputVector, const size_t numOfOperands, ...);
 void differenceOfVec3(Vec3 outputVector, const size_t numOfOperands, ...);
