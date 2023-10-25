4a5d7d223eaf0371a2571fdb900147fc1d1af506
Alvin21Bon <alvin21bon@gmail.com>
2023-10-25 00:55:13 -0400

    implemented sumOfVec4

--- src/vec4.c
+++ src/vec4.c
@@ -27,7 +27,21 @@ void subtractVec4(const Vec4 vectorA, const Vec4 vectorB, Vec4 outputVector)
 	outputVector[3] = vectorA[3] - vectorB[3];
 }
 
-void sumOfVec4(Vec4 outputVector, const size_t numOfOperands, ...);
+void sumOfVec4(Vec4 outputVector, const size_t numOfOperands, ...)
+{
+	int argIndex;
+	va_list argInfo;
+
+	va_start(argInfo, numOfOperands);
+
+	zeroVec4(outputVector);
+	for (argIndex = 0; argIndex < numOfOperands; argIndex++)
+	{
+		addVec4(outputVector, va_arg(argInfo, Vec4), outputVector);
+	}
+
+	va_end(argInfo);
+}
 void differenceOfVec4(Vec4 outputVector, const size_t numOfOperands, ...);
 
 void scaleVec4(const float scalar, const Vec4 inputVector, Vec4 outputVector)
