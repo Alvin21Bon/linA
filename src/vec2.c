a23ac68aed84ba9ba9170e45ee02dcdd16dcc80b
Alvin21Bon <alvin21bon@gmail.com>
2023-10-25 00:22:53 -0400

    scaler function

--- src/vec2.c
+++ src/vec2.c
@@ -20,6 +20,10 @@ void subtractVec2(const Vec2 vectorA, const Vec2 vectorB, Vec2 outputVector)
 void sumOfVec2(Vec2 outputVector, const size_t numOfOperands, ...);
 void differenceOfVec2(Vec2 outputVector, const size_t numOfOperands, ...);
 
-void scaleVec2(const float scalar, const Vec2 inputVector, Vec2 ouputVector);
+void scaleVec2(const float scalar, const Vec2 inputVector, Vec2 outputVector)
+{
+	outputVector[0] = scalar * inputVector[0];
+	outputVector[1] = scalar * inputVector[1];
+}
 void negVec2(const Vec2 inputVector, Vec2 outputVector);
 
