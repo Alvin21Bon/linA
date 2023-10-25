3852e682954c7717363810b28d6b302444097598
Alvin21Bon <alvin21bon@gmail.com>
2023-10-25 00:32:33 -0400

    add and subtract functions

--- src/vec4.c
+++ src/vec4.c
@@ -8,8 +8,20 @@ void fillVec4(const float x, const float y, const float z, const float w, Vec4 o
 	outputVector[3] = w;
 }
 
-void addVec4(const Vec4 vectorA, const Vec4 vectorB, Vec4 outputVector);
-void subtractVec4(const Vec4 vectorA, const Vec4 vectorB, Vec4 outputVector);
+void addVec4(const Vec4 vectorA, const Vec4 vectorB, Vec4 outputVector)
+{
+	outputVector[0] = vectorA[0] + vectorB[0];
+	outputVector[1] = vectorA[1] + vectorB[1];
+	outputVector[2] = vectorA[2] + vectorB[2];
+	outputVector[3] = vectorA[3] + vectorB[3];
+}
+void subtractVec4(const Vec4 vectorA, const Vec4 vectorB, Vec4 outputVector)
+{
+	outputVector[0] = vectorA[0] - vectorB[0];
+	outputVector[1] = vectorA[1] - vectorB[1];
+	outputVector[2] = vectorA[2] - vectorB[2];
+	outputVector[3] = vectorA[3] - vectorB[3];
+}
 
 void sumOfVec4(Vec4 outputVector, const size_t numOfOperands, ...);
 void differenceOfVec4(Vec4 outputVector, const size_t numOfOperands, ...);
