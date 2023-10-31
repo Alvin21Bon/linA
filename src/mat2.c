2b132c2bafbe4c2ee5d1f06763897a4aaef8302b
Alvin21Bon <alvin21bon@gmail.com>
2023-10-31 11:13:08 -0400

    intermediatry commit

--- src/mat2.c
+++ src/mat2.c
@@ -1,10 +1,21 @@
 #include "../include/aglm.h"
 
-void fillMat2(const Vec2 column1, const Vec2 column2, Mat2 outputMatrix);
+void fillMat2(const Vec2 column1, const Vec2 column2, Mat2 outputMatrix)
+{
+	copyVec2(column1, outputMatrix[0]);
+	copyVec2(column2, outputMatrix[1]);
+}
 
 void fillMat2WithFloats(const float col1row1, const float col2row1, 
 			const float col1row2, const float col2row2,
-			Mat2 outputMatrix);
+			Mat2 outputMatrix)
+{
+	Vec2 column1, column2;
+	fillVec2(col1row1, col1row2, column1);
+	fillVec2(col2row1, col2row2, column2);
+
+	fillMat2(column1, column2, outputMatrix);
+}
 
 void zeroMat2(Mat2 zeroedMatrix);
 void copyMat2(const Mat2 inputMatrix, Mat2 copyIntoMatrix);
