ac76faa215f8359ea78b1eb8bbc7909852c79be9
Alvin21Bon <alvin21bon@gmail.com>
2023-10-31 09:18:19 -0400

    header for Mat3

--- include/mat3.h
+++ include/mat3.h
@@ -6,4 +6,28 @@
 
 typedef Vec3 Mat3[3];
 
+void fillMat3(const Vec3 column1, const Vec3 column2, const Vec3 column3, Mat3 outputMatrix);
+
+// Arguments are in ROW-MAJOR order. Remember to transpose resulting matrix.
+void fillMat3WithFloats(const float col1row1, const float col2row1, const float col3row1, 
+			const float col1row2, const float col2row2, const float col3row2,
+			const float col1row3, const float col2row3, const float col3row3,
+			Mat3 outputMatrix);
+
+void zeroMat3(Mat3 zeroedMatrix);
+void copyMat3(const Mat3 inputMatrix, Mat3 copyIntoMatrix);
+void fillIdentityMat3(Mat3 outputMatrix);
+void transposeMat3(Mat3 transposedMatrix);
+
+void multMat3Vec(const Mat3 transformedSpace, Vec3 transformedVector);
+
+/*
+ * This function allows for a variable amount of vectors to be multiplied. To use the
+ * function, simply add as many vector arguments as you want, while specifying the
+ * number of vectors.
+*/
+void productOfMat3Vecs(const Mat3 transformedSpace, const size_t numOfVectors, ...);
+
+void multMat3Mat(const Mat3 transformedSpace, Mat3 transformedMatrix);
+
 #endif // AGLM_MAT3_H
