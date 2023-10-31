66b343d4c8618f7179e8c8448b908e35aa59b63f
Alvin21Bon <alvin21bon@gmail.com>
2023-10-31 09:14:05 -0400

    header file for Mat2

--- include/mat2.h
+++ include/mat2.h
@@ -6,4 +6,27 @@
 
 typedef Vec2 Mat2[2];
 
+void fillMat2(const Vec2 column1, const Vec2 column2, Mat2 outputMatrix);
+
+// Arguments are in ROW-MAJOR order. Remember to transpose resulting matrix.
+void fillMat2WithFloats(const float col1row1, const float col2row1, 
+			const float col1row2, const float col2row2,
+			Mat2 outputMatrix);
+
+void zeroMat2(Mat2 zeroedMatrix);
+void copyMat2(const Mat2 inputMatrix, Mat2 copyIntoMatrix);
+void fillIdentityMat2(Mat2 outputMatrix);
+void transposeMat2(Mat2 transposedMatrix);
+
+void multMat2Vec(const Mat2 transformedSpace, Vec2 transformedVector);
+
+/*
+ * This function allows for a variable amount of vectors to be multiplied. To use the
+ * function, simply add as many vector arguments as you want, while specifying the
+ * number of vectors.
+*/
+void productOfMat2Vecs(const Mat2 transformedSpace, const size_t numOfVectors, ...);
+
+void multMat2Mat(const Mat2 transformedSpace, Mat2 transformedMatrix);
+
 #endif // AGLM_MAT2_H
