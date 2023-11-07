d90cff841aa0506ea427bc53c23c0de04ab52b68
Alvin21Bon <alvin21bon@gmail.com>
2023-11-07 11:42:52 -0500

    all transformation functions for mat2

--- include/transform-mat2.h
+++ include/transform-mat2.h
@@ -4,4 +4,55 @@
 #include "shared.h"
 #include "mat2.h"
 
+/******************************************************************************
+* SCALING FUNCTIONS
+******************************************************************************/
+
+//
+void scaleXMat2(Mat2 matrixToTransform, const float scaleAmt);
+void scaleYMat2(Mat2 matrixToTransform, const float scaleAmt);
+
+/******************************************************************************
+* ROTATING FUNCTIONS
+*
+* NOTE: All of these functions use radians
+******************************************************************************/
+
+//
+void rotateXMat2(Mat2 matrixToTransform, const double angleInRadians);
+void rotateYMat2(Mat2 matrixToTransform, const double angleInRadians);
+void rotateZMat2(Mat2 matrixToTransform, const double angleInRadians);
+
+void pitchMat2(Mat2 matrixToTransform, const double angleInRadians);
+void yawMat2(Mat2 matrixToTransform, const double angleInRadians);
+void rollMat2(Mat2 matrixToTransform, const double angleInRadians);
+
+/******************************************************************************
+* REFLECTING FUNCTIONS
+******************************************************************************/
+
+//
+void reflectPlaneYZMat2(Mat2 matrixToTransform); 
+void reflectPlaneZYMat2(Mat2 matrixToTransform);
+
+void reflectPlaneZXMat2(Mat2 matrixToTransform);
+void reflectPlaneXZMat2(Mat2 matrixToTransform);
+
+void reflectX(Mat2 matrixToTransform);
+void reflectY(Mat2 matrixToTransform);
+
+/******************************************************************************
+* SHEARING FUNCTIONS
+******************************************************************************/
+
+//
+void shearPlaneYZMat2(Mat2 matrixToTransform, const float shearAmt);
+void shearPlaneZYMat2(Mat2 matrixToTransform, const float shearAmt);
+
+void shearPlaneZXMat2(Mat2 matrixToTransform, const float shearAmt);
+void shearPlaneXZMat2(Mat2 matrixToTransform, const float shearAmt);
+
+void shearX(Mat2 matrixToTransform, const float shearAmt);
+void shearY(Mat2 matrixToTransform, const float shearAmt);
+
 #endif // AGLM_TRANSFORM_MAT2_H
