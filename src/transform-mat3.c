7356594751d8c27ca44b3c0d17fa3fc74234947c
Alvin21Bon <alvin21bon@gmail.com>
2023-11-09 16:37:15 -0500

    some mat3 implementation. commit before testing rotation

--- src/transform-mat3.c
+++ src/transform-mat3.c
@@ -1,12 +1,66 @@
 #include "../include/aglm.h"
 
-void scaleXMat3(Mat3 matrixToTransform, const float scaleAmt);
-void scaleYMat3(Mat3 matrixToTransform, const float scaleAmt);
-void scaleZMat3(Mat3 matrixToTransform, const float scaleAmt);
+void scaleXMat3(Mat3 matrixToTransform, const float scaleAmt)
+{
+	Mat3 transformation;
+	fillIdentityMat3(transformation);
 
-void rotateXMat3(Mat3 matrixToTransform, const double angleInRadians);
-void rotateYMat3(Mat3 matrixToTransform, const double angleInRadians);
-void rotateZMat3(Mat3 matrixToTransform, const double angleInRadians);
+	scaleVec3(scaleAmt, transformation[0]);
+
+	multMat3Mat(transformation, matrixToTransform);
+}
+void scaleYMat3(Mat3 matrixToTransform, const float scaleAmt)
+{
+	Mat3 transformation;
+	fillIdentityMat3(transformation);
+
+	scaleVec3(scaleAmt, transformation[1]);
+
+	multMat3Mat(transformation, matrixToTransform);
+}
+void scaleZMat3(Mat3 matrixToTransform, const float scaleAmt)
+{
+	Mat3 transformation;
+	fillIdentityMat3(transformation);
+
+	scaleVec3(scaleAmt, transformation[2]);
+
+	multMat3Mat(transformation, matrixToTransform);
+}
+
+void rotateXMat3(Mat3 matrixToTransform, const double angleInRadians)
+{
+	Mat3 transformation;
+	Vec3 transformedIHat = {1, 0, 0};
+	Vec3 transformedJHat = {0, cos(angleInRadians), sin(angleInRadians)};
+	Vec3 transformedKHat = {0, -sin(angleInRadians), cos(angleInRadians)};
+
+	fillMat3(transformedIHat, transformedJHat, transformedKHat, transformation);
+
+	multMat3Mat(transformation, matrixToTransform);
+}
+void rotateYMat3(Mat3 matrixToTransform, const double angleInRadians)
+{
+	Mat3 transformation;
+	Vec3 transformedIHat = {cos(angleInRadians), 0, -sin(angleInRadians)};
+	Vec3 transformedJHat = {0, 1, 0};
+	Vec3 transformedKHat = {sin(angleInRadians), 0, cos(angleInRadians)};
+
+	fillMat3(transformedIHat, transformedJHat, transformedKHat, transformation);
+
+	multMat3Mat(transformation, matrixToTransform);
+}
+void rotateZMat3(Mat3 matrixToTransform, const double angleInRadians)
+{
+	Mat3 transformation;
+	Vec3 transformedIHat = {cos(angleInRadians), sin(angleInRadians), 0};
+	Vec3 transformedJHat = {-sin(angleInRadians), cos(angleInRadians), 0};
+	Vec3 transformedKHat = {0, 0, 1};
+
+	fillMat3(transformedIHat, transformedJHat, transformedKHat, transformation);
+
+	multMat3Mat(transformation, matrixToTransform);
+}
 
 void pitchMat3(Mat3 matrixToTransform, const double angleInRadians);
 void yawMat3(Mat3 matrixToTransform, const double angleInRadians);
