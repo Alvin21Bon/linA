2422ba60bea223de3853d9b25fd4d6bfdb8cb750
Alvin21Bon <alvin21bon@gmail.com>
2023-11-09 19:33:51 -0500

    rotation functions for mat4

--- src/transform-mat4.c
+++ src/transform-mat4.c
@@ -28,9 +28,42 @@ void scaleZMat4(Mat4 matrixToTransform, const float scaleAmt)
 	multMat4Mat(transformation, matrixToTransform);
 }
 
-void rotateXMat4(Mat4 matrixToTransform, const double angleInRadians);
-void rotateYMat4(Mat4 matrixToTransform, const double angleInRadians);
-void rotateZMat4(Mat4 matrixToTransform, const double angleInRadians);
+void rotateXMat4(Mat4 matrixToTransform, const double angleInRadians)
+{
+	Mat4 transformation;
+	Vec4 transformedIHat = {1, 0, 0, 0};
+	Vec4 transformedJHat = {0, cos(angleInRadians), sin(angleInRadians), 0};
+	Vec4 transformedKHat = {0, -sin(angleInRadians), cos(angleInRadians), 0};
+	Vec4 transformedLHat = {0, 0, 0, 1};
+
+	fillMat4(transformedIHat, transformedJHat, transformedKHat, transformedLHat, transformation);
+
+	multMat4Mat(transformation, matrixToTransform);
+}
+void rotateYMat4(Mat4 matrixToTransform, const double angleInRadians)
+{
+	Mat4 transformation;
+	Vec4 transformedIHat = {cos(angleInRadians), 0, -sin(angleInRadians), 0};
+	Vec4 transformedJHat = {0, 1, 0, 0};
+	Vec4 transformedKHat = {sin(angleInRadians), 0, cos(angleInRadians), 0};
+	Vec4 transformedLHat = {0, 0, 0, 1};
+
+	fillMat4(transformedIHat, transformedJHat, transformedKHat, transformedLHat, transformation);
+
+	multMat4Mat(transformation, matrixToTransform);
+}
+void rotateZMat4(Mat4 matrixToTransform, const double angleInRadians)
+{
+	Mat4 transformation;
+	Vec4 transformedIHat = {cos(angleInRadians), sin(angleInRadians), 0, 0};
+	Vec4 transformedJHat = {-sin(angleInRadians), cos(angleInRadians), 0, 0};
+	Vec4 transformedKHat = {0, 0, 1, 0};
+	Vec4 transformedLHat = {0, 0, 0, 1};
+
+	fillMat4(transformedIHat, transformedJHat, transformedKHat, transformedLHat, transformation);
+
+	multMat4Mat(transformation, matrixToTransform);
+}
 
 void pitchMat4(Mat4 matrixToTransform, const double angleInRadians);
 void yawMat4(Mat4 matrixToTransform, const double angleInRadians);
