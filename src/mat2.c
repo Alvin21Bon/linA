c7508c794bba33b2598afd98b05a8110c0eeb79a
Alvin21Bon <alvin21bon@gmail.com>
2023-12-10 13:35:01 -0500

    new matrix src files outline

--- src/mat2.c
+++ src/mat2.c
@@ -1,90 +1,17 @@
 #include "../include/lina.h"
 
-void fillMat2(const Vec2 column0, const Vec2 column1, Mat2 outputMatrix)
-{
-	copyVec2(column0, outputMatrix[0]);
-	copyVec2(column1, outputMatrix[1]);
-}
+// constructors
+Mat2 mat2(const Vec2 col0, const Vec2 col1);
+Mat2 mat2Fill(const float fillValue);
+Mat2 mat2Zero();
+Mat2 mat2Diagonalize(const float diagonalValue);
+Mat2 mat2Identity();
 
-void fillMat2WithFloats(const float col0row0, const float col1row0, 
-			const float col0row1, const float col1row1,
-			Mat2 outputMatrix)
-{
-	Vec2 column0, column1;
-	fillVec2(col0row0, col0row1, column0);
-	fillVec2(col1row0, col1row1, column1);
+// operations
+Mat2 mat2MultVec(const Mat2 mat, const Vec2 vec);
+Mat2 mat2MultMat(const Mat2 left, const Mat2 right);
+Mat2 mat2Inverse(const Mat2 mat);
 
-	fillMat2(column0, column1, outputMatrix);
-}
-
-void zeroMat2(Mat2 zeroedMatrix)
-{
-	memset(zeroedMatrix, 0, sizeof(float) * MAT2_NUM_OF_ELEMENTS);
-}
-void copyMat2(const Mat2 inputMatrix, Mat2 copyIntoMatrix)
-{
-	fillMat2(inputMatrix[0], inputMatrix[1], copyIntoMatrix);
-}
-void fillIdentityMat2(Mat2 outputMatrix)
-{
-	zeroMat2(outputMatrix);
-
-	for (int diagonalPos = 0; diagonalPos < MAT2_NUM_OF_COLUMNS && diagonalPos < MAT2_NUM_OF_ROWS; diagonalPos++)
-	{
-		outputMatrix[diagonalPos][diagonalPos] = 1;
-	}
-}
-void transposeMat2(Mat2 transposedMatrix)
-{
-	fillMat2WithFloats(transposedMatrix[0][0], transposedMatrix[0][1], 
-			   transposedMatrix[1][0], transposedMatrix[1][1],
-			   transposedMatrix);
-}
-
-void multMat2Vec(const Mat2 transformedSpace, Vec2 transformedVector)
-{
-	Mat2 tempCopyOfMatrix;
-	copyMat2(transformedSpace, tempCopyOfMatrix);
-
-	for (int columnIndex = 0; columnIndex < MAT2_NUM_OF_COLUMNS; columnIndex++)
-	{
-		scaleVec2(transformedVector[columnIndex], tempCopyOfMatrix[columnIndex]);
-	}
-
-	sumOfVec2(transformedVector, 2, tempCopyOfMatrix[0], tempCopyOfMatrix[1]);
-}
-
-void productOfMat2Vecs(const Mat2 transformedSpace, const size_t numOfVectors, ...)
-{
-	int argIndex;
-	va_list argInfo;
-
-	va_start(argInfo, numOfVectors);
-
-	for (argIndex = 0; argIndex < numOfVectors; argIndex++)
-	{
-		multMat2Vec(transformedSpace, va_arg(argInfo, float*));
-	}
-
-	va_end(argInfo);
-}
-
-void multMat2Mat(const Mat2 transformedSpace, Mat2 transformedMatrix)
-{
-	productOfMat2Vecs(transformedSpace, 2, transformedMatrix[0], transformedMatrix[1]);
-}
-
-void printMat2(const Mat2 matrixToPrint)
-{
-	for (int rowIndex = 0; rowIndex < MAT2_NUM_OF_ROWS; rowIndex++)
-	{
-		printf("| ");
-
-		for (int columnIndex = 0; columnIndex < MAT2_NUM_OF_COLUMNS; columnIndex++)
-		{
-			printf("%f ", matrixToPrint[columnIndex][rowIndex]);
-		}
-
-		printf("|\n");
-	}
-}
+// matrix info
+float mat2Determinant(const Mat2 mat);
+void mat2Print(const Mat2 mat);
--- src/mat3.c
+++ src/mat3.c
@@ -1,94 +1,17 @@
 #include "../include/lina.h"
 
-void fillMat3(const Vec3 column0, const Vec3 column1, const Vec3 column2, Mat3 outputMatrix)
-{
-	copyVec3(column0, outputMatrix[0]);
-	copyVec3(column1, outputMatrix[1]);
-	copyVec3(column2, outputMatrix[2]);
-}
+// constructors
+Mat3 mat3(const Vec3 col0, const Vec3 col1, const Vec3 col2);
+Mat3 mat3Fill(const float fillValue);
+Mat3 mat3Zero();
+Mat3 mat3Diagonalize(const float diagonalValue);
+Mat3 mat3Identity();
 
-void fillMat3WithFloats(const float col0row0, const float col1row0, const float col2row0, 
-			const float col0row1, const float col1row1, const float col2row1,
-			const float col0row2, const float col1row2, const float col2row2,
-			Mat3 outputMatrix)
-{
-	Vec3 column0, column1, column2;
-	fillVec3(col0row0, col0row1, col0row2, column0);
-	fillVec3(col1row0, col1row1, col1row2, column1);
-	fillVec3(col2row0, col2row1, col2row2, column2);
+// operations
+Mat3 mat3MultVec(const Mat3 mat, const Vec3 vec);
+Mat3 mat3MultMat(const Mat3 left, const Mat3 right);
+Mat3 mat3Inverse(const Mat3 mat);
 
-	fillMat3(column0, column1, column2, outputMatrix);
-}
-
-void zeroMat3(Mat3 zeroedMatrix)
-{
-	memset(zeroedMatrix, 0, sizeof(float) * MAT3_NUM_OF_ELEMENTS);
-}
-void copyMat3(const Mat3 inputMatrix, Mat3 copyIntoMatrix)
-{
-	fillMat3(inputMatrix[0], inputMatrix[1], inputMatrix[2], copyIntoMatrix);
-}
-void fillIdentityMat3(Mat3 outputMatrix)
-{
-	zeroMat3(outputMatrix);
-
-	for (int diagonalPos = 0; diagonalPos < MAT3_NUM_OF_COLUMNS && diagonalPos < MAT3_NUM_OF_ROWS; diagonalPos++)
-	{
-		outputMatrix[diagonalPos][diagonalPos] = 1;
-	}
-}
-void transposeMat3(Mat3 transposedMatrix)
-{
-	fillMat3WithFloats(transposedMatrix[0][0], transposedMatrix[0][1], transposedMatrix[0][2],
-			   transposedMatrix[1][0], transposedMatrix[1][1], transposedMatrix[1][2],
-			   transposedMatrix[2][0], transposedMatrix[2][1], transposedMatrix[2][2],
-			   transposedMatrix);
-}
-
-void multMat3Vec(const Mat3 transformedSpace, Vec3 transformedVector)
-{
-	Mat3 tempCopyOfMatrix;
-	copyMat3(transformedSpace, tempCopyOfMatrix);
-
-	for (int columnIndex = 0; columnIndex < MAT3_NUM_OF_COLUMNS; columnIndex++)
-	{
-		scaleVec3(transformedVector[columnIndex], tempCopyOfMatrix[columnIndex]);
-	}
-
-	sumOfVec3(transformedVector, 3, tempCopyOfMatrix[0], tempCopyOfMatrix[1], tempCopyOfMatrix[2]);
-}
-
-void productOfMat3Vecs(const Mat3 transformedSpace, const size_t numOfVectors, ...)
-{
-	int argIndex;
-	va_list argInfo;
-
-	va_start(argInfo, numOfVectors);
-
-	for (argIndex = 0; argIndex < numOfVectors; argIndex++)
-	{
-		multMat3Vec(transformedSpace, va_arg(argInfo, float*));
-	}
-
-	va_end(argInfo);
-}
-
-void multMat3Mat(const Mat3 transformedSpace, Mat3 transformedMatrix)
-{
-	productOfMat3Vecs(transformedSpace, 3, transformedMatrix[0], transformedMatrix[1], transformedMatrix[2]);
-}
-
-void printMat3(const Mat3 matrixToPrint)
-{
-	for (int rowIndex = 0; rowIndex < MAT3_NUM_OF_ROWS; rowIndex++)
-	{
-		printf("| ");
-
-		for (int columnIndex = 0; columnIndex < MAT3_NUM_OF_COLUMNS; columnIndex++)
-		{
-			printf("%f ", matrixToPrint[columnIndex][rowIndex]);
-		}
-
-		printf("|\n");
-	}
-}
+// matrix info
+float mat3Determinant(const Mat3 mat);
+void mat3Print(const Mat3 mat);
