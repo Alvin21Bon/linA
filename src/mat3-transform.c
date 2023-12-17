ea1185a0f9c651594f0771714b00c5cc0cb4eae1
Alvin21Bon <alvin21bon@gmail.com>
2023-12-17 02:12:05 -0500

    mat3 rotating and reflecting

--- src/mat3-transform.c
+++ src/mat3-transform.c
@@ -26,20 +26,55 @@ Mat3 mat3Scaled(const Mat3 mat, const float scalar)
 }
 
 // rotating
-Mat3 mat3Rotated(const Mat3 mat, const double radians, const Vec3 axis);
-Mat3 mat3Pitch(const Mat3 mat, const double radians);
-Mat3 mat3Yaw(const Mat3 mat, const double radians);
-Mat3 mat3Roll(const Mat3 mat, const double radians);
+Mat3 mat3Rotated(const Mat3 mat, const double radians, const Vec3 axis)
+{
+	Mat3 transformation = mat3Identity();
+	transformation.ihat = vec3Rotated(transformation.ihat, radians, axis);
+	transformation.jhat = vec3Rotated(transformation.jhat, radians, axis);
+	transformation.khat = vec3Rotated(transformation.khat, radians, axis);
+
+	return mat3MultMat(transformation, mat);
+}
+Mat3 mat3Pitch(const Mat3 mat, const double radians)
+{
+	Mat3 transformation = mat3Rotated(mat3Identity(), radians, vec3(1,0,0));
+	return mat3MultMat(transformation, mat);
+}
+Mat3 mat3Yaw(const Mat3 mat, const double radians)
+{
+	Mat3 transformation = mat3Rotated(mat3Identity(), radians, vec3(0,1,0));
+	return mat3MultMat(transformation, mat);
+}
+Mat3 mat3Roll(const Mat3 mat, const double radians)
+{
+	Mat3 transformation = mat3Rotated(mat3Identity(), radians, vec3(0,0,1));
+	return mat3MultMat(transformation, mat);
+}
 
 // reflecting
-Mat3 mat3ReflectedYZ(const Mat3 mat);
-Mat3 mat3ReflectedZY(const Mat3 mat);
+Mat3 mat3ReflectedYZ(const Mat3 mat)
+{
+	Mat3 transformation = mat3Identity();
+	transformation.ihat = vec3Negated(transformation.ihat);
+	return mat3MultMat(transformation, mat);
+}
+Mat3 mat3ReflectedZY(const Mat3 mat) { return mat3ReflectedYZ(mat); }
 
-Mat3 mat3ReflectedZX(const Mat3 mat);
-Mat3 mat3ReflectedXZ(const Mat3 mat);
+Mat3 mat3ReflectedZX(const Mat3 mat)
+{
+	Mat3 transformation = mat3Identity();
+	transformation.jhat = vec3Negated(transformation.jhat);
+	return mat3MultMat(transformation, mat);
+}
+Mat3 mat3ReflectedXZ(const Mat3 mat) { return mat3ReflectedZX(mat); }
 
-Mat3 mat3ReflectedXY(const Mat3 mat);
-Mat3 mat3ReflectedYX(const Mat3 mat);
+Mat3 mat3ReflectedXY(const Mat3 mat)
+{
+	Mat3 transformation = mat3Identity();
+	transformation.khat = vec3Negated(transformation.khat);
+	return mat3MultMat(transformation, mat);
+}
+Mat3 mat3ReflectedYX(const Mat3 mat) { return mat3ReflectedXY(mat); }
 
 // shearing
 Mat3 mat3ShearedYZ(const Mat3 mat, const float shearAmtY, const float shearAmtZ);
