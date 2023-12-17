3631f1aa9a4f832f70d4ef4a8b72fc923937bf0f
Alvin21Bon <alvin21bon@gmail.com>
2023-12-17 02:00:47 -0500

    implemented mat3 scaling functions

--- src/mat3-transform.c
+++ src/mat3-transform.c
@@ -1,10 +1,29 @@
 #include "../include/lina.h"
 
 // scaling
-Mat3 mat3ScaledX(const Mat3 mat, const float scalar);
-Mat3 mat3ScaledY(const Mat3 mat, const float scalar);
-Mat3 mat3ScaledZ(const Mat3 mat, const float scalar);
-Mat3 mat3Scaled(const Mat3 mat, const float scalar);
+Mat3 mat3ScaledX(const Mat3 mat, const float scalar)
+{
+	Mat3 transformation = mat3Identity();
+	transformation.ihat.x = scalar;
+	return mat3MultMat(transformation, mat);
+}
+Mat3 mat3ScaledY(const Mat3 mat, const float scalar)
+{
+	Mat3 transformation = mat3Identity();
+	transformation.jhat.y = scalar;
+	return mat3MultMat(transformation, mat);
+}
+Mat3 mat3ScaledZ(const Mat3 mat, const float scalar)
+{
+	Mat3 transformation = mat3Identity();
+	transformation.khat.z = scalar;
+	return mat3MultMat(transformation, mat);
+}
+Mat3 mat3Scaled(const Mat3 mat, const float scalar)
+{
+	Mat3 transformation = mat3Diagonalize(scalar);
+	return mat3MultMat(transformation, mat);
+}
 
 // rotating
 Mat3 mat3Rotated(const Mat3 mat, const double radians, const Vec3 axis);
