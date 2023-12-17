1698ff1a94c6e8ad77fa4712e305e8861dfd8027
Alvin21Bon <alvin21bon@gmail.com>
2023-12-17 01:39:48 -0500

    implemented scaling and rotation functions for mat2

--- src/mat2-transform.c
+++ src/mat2-transform.c
@@ -1,19 +1,57 @@
 #include "../include/lina.h"
 
 // scaling
-Mat2 mat2ScaledX(const Mat2 mat, const float scalar);
-Mat2 mat2ScaledY(const Mat2 mat, const float scalar);
-Mat2 mat2Scaled(const Mat2 mat, const float scalar);
+Mat2 mat2ScaledX(const Mat2 mat, const float scalar)
+{
+	Mat2 transformation = mat2Identity();
+	transformation.ihat.x = scalar;
+
+	return mat2MultMat(transformation, mat);
+}
+Mat2 mat2ScaledY(const Mat2 mat, const float scalar)
+{
+	Mat2 transformation = mat2Identity();
+	transformation.jhat.y = scalar;
+
+	return mat2MultMat(transformation, mat);
+}
+Mat2 mat2Scaled(const Mat2 mat, const float scalar)
+{
+	Mat2 transformation = mat2Identity();
+	transformation = mat2ScaledX(transformation, scalar);
+	transformation = mat2ScaledY(transformation, scalar);
+
+	return mat2MultMat(transformation, mat);
+}
 
 // rotating
-Mat2 mat2Rotated(const Mat2 mat, const double radians, const Vec3 axis);
-Mat2 mat2RotatedX(const Mat2 mat, const double radians);
-Mat2 mat2RotatedY(const Mat2 mat, const double radians);
-Mat2 mat2RotatedZ(const Mat2 mat, const double radians);
+Mat2 mat2Rotated(const Mat2 mat, const double radians, const Vec3 axis)
+{
+	Mat2 transformation = mat2Identity();
+	transformation.ihat = vec2Rotated(transformation.ihat, radians, axis);
+	transformation.jhat = vec2Rotated(transformation.jhat, radians, axis);
 
-Mat2 mat2Pitch(const Mat2 mat, const double radians);
-Mat2 mat2Yaw(const Mat2 mat, const double radians);
-Mat2 mat2Roll(const Mat2 mat, const double radians);
+	return mat2MultMat(transformation, mat);
+}
+Mat2 mat2RotatedX(const Mat2 mat, const double radians)
+{
+	Mat2 transformation = mat2Rotated(mat2Identity(), radians, vec3(1,0,0));
+	return mat2MultMat(transformation, mat);
+}
+Mat2 mat2RotatedY(const Mat2 mat, const double radians)
+{
+	Mat2 transformation = mat2Rotated(mat2Identity(), radians, vec3(0,1,0));
+	return mat2MultMat(transformation, mat);
+}
+Mat2 mat2RotatedZ(const Mat2 mat, const double radians)
+{
+	Mat2 transformation = mat2Rotated(mat2Identity(), radians, vec3(0,0,1));
+	return mat2MultMat(transformation, mat);
+}
+
+Mat2 mat2Pitch(const Mat2 mat, const double radians) { return mat2RotatedX(mat, radians); }
+Mat2 mat2Yaw(const Mat2 mat, const double radians) { return mat2RotatedY(mat, radians); }
+Mat2 mat2Roll(const Mat2 mat, const double radians) { return mat2RotatedZ(mat, radians); }
 
 // reflecting
 Mat2 mat2ReflectedYZ(const Mat2 mat);
