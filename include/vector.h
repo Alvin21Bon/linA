7a5b3f7a525eb6287d5d4ed106ac22a476183c9b
Alvin21Bon <alvin21bon@gmail.com>
2024-09-26 01:46:15 -0400

    now include the functions for the different vector types

--- include/vector.h
+++ include/vector.h
@@ -3,7 +3,16 @@
 
 #include "vec-types.h"
 #include "vec2/vec2.h"
+#include "vec2/uvec2.h"
+#include "vec2/ivec2.h"
+#include "vec2/dvec2.h"
 #include "vec3/vec3.h"
+#include "vec3/uvec3.h"
+#include "vec3/ivec3.h"
+#include "vec3/dvec3.h"
 #include "vec4/vec4.h"
+#include "vec4/uvec4.h"
+#include "vec4/ivec4.h"
+#include "vec4/dvec4.h"
 
 #endif // LINA_VECTOR_H
