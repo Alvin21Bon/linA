bac156bfe3fc1c563e848b5d22d017ed745ba984
Alvin21Bon <alvin21bon@gmail.com>
2024-09-26 02:13:28 -0400

    incldued double matrix functions

--- include/matrix.h
+++ include/matrix.h
@@ -3,7 +3,10 @@
 
 #include "mat-types.h"
 #include "mat2/mat2.h"
+#include "mat2/dmat2.h"
 #include "mat3/mat3.h"
+#include "mat3/dmat3.h"
 #include "mat4/mat4.h"
+#include "mat4/dmat4.h"
 
 #endif // LINA_MATRIX_H
